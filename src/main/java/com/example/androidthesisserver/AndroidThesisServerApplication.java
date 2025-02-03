66f03620f8d03d5a0601c43c90060654c0c0dac2
tcyyanxi <1972228514@qq.com>
2025-02-03 20:15:35 +0800

    Initial commit

---
+++ src/main/java/com/example/androidthesisserver/AndroidThesisServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.androidthesisserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidThesisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidThesisServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/androidthesisserver/AndroidThesisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.androidthesisserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidThesisServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
